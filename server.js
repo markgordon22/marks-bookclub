30a023c54c88fcaca9f958fad97c7b98d09a93de
markgordon22 <72653213+markgordon22@users.noreply.github.com>
2021-12-23 17:29:27 +0000

    books create/index/new.ejs

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const app = express()
 const expressLayouts = require("express-ejs-layouts")
 const indexRouter = require("./routes/index")
 const authorRouter = require("./routes/authors")
-
+const bookRouter = require('./routes/books')
 
 app.set('view engine', 'ejs')
 app.set('views', __dirname + '/views')
@@ -27,6 +27,7 @@ db.once("open", () => console.log("worked"))
 
 app.use('/', indexRouter)
 app.use('/authors', authorRouter)
+app.use('/books', bookRouter)
 
 app.listen(process.env.PORT || 3000)
 
